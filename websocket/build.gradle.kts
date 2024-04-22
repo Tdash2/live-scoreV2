744d3975b7e5b220fc714b180c65eda97e811653
Ripley White <pearlcodes@outlook.com>
2024-04-22 01:06:33 +0100

    Added dependencies and Logger config.

--- websocket/build.gradle.kts
+++ websocket/build.gradle.kts
@@ -1,5 +1,8 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     id("java")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "org.aquastreams.livescore.ws"
@@ -10,9 +13,27 @@ repositories {
 }
 
 dependencies {
+    implementation("org.glassfish.tyrus:tyrus-server:1.1") // yes, this is old.
+    implementation("org.glassfish.tyrus:tyrus-container-grizzly:1.1") // this too
 
+    implementation("redis.clients:jedis:2.8.0")
+
+    compileOnly("org.projectlombok:lombok:1.18.32")
+    annotationProcessor("org.projectlombok:lombok:1.18.32")
+
+    implementation("org.slf4j:slf4j-api:2.0.13")
+    implementation("ch.qos.logback:logback-core:1.5.5")
+    implementation("ch.qos.logback:logback-classic:1.5.5")
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks.withType<ShadowJar> {
+    archiveBaseName.set("livescore-websocket")
+    archiveClassifier.set("")
+    archiveVersion.set("")
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "org.aquastreams.livescore.ws.Main"
+    }
 }
--- websocket/src/main/java/org/aquastreams/livescore/ws/Main.java
+++ websocket/src/main/java/org/aquastreams/livescore/ws/Main.java
@@ -1,7 +1,10 @@
 package org.aquastreams.livescore.ws;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        log.info("Starting AquaStreams Bug WebSocket...");
     }
 }
